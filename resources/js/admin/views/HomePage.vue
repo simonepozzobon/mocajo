03ca9dd5239634b6f32533f54928197b3731053f
Angus Simons <info@simonepozzobon.com>
2019-02-27 01:51:35 +0100

    Aggiornata homepage con il sitema ricursivo

--- resources/js/admin/views/HomePage.vue
+++ resources/js/admin/views/HomePage.vue
@@ -1,43 +1,24 @@
 <template lang="html">
     <page-template title="Homepage">
-        <div class="form">
-            <div class="form-group row">
-                <label for="video" class="col-md-3">Cambia video</label>
-                <div class="input-group mb-3 col-md-9">
-                    <div class="custom-file">
-                        <input @change="filesChange($event.target.name, $event.target.files)" type="file" class="form-control-file" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" ref="input">
-                        <div class="valid-feedback">
-                            File corretto, clicca salva
-                        </div>
-                        <div class="invalid-feedback">
-                            {{ errorMessage }}
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group row pb-5">
-                <div class="col-md-9 offset-md-3">
-                    <a href="#" class="btn btn-primary" @click="upload">
-                        Salva
-                    </a>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="videoPreview" class="col-md-3">Video Attuale</label>
-                <div class="col-md-9">
-                    <video-player
-                        class="video-player-box img-fluid"
-                        ref="videoPlayer"
-                        :options="playerOptions"
-                        :playsinline="true"/>
-                </div>
-            </div>
-        </div>
+        <seo-group>
+
+        </seo-group>
+        <page-section
+            title="Video Principale"
+            idx="video"
+            :disable-title="true"
+            :disable-txt="true"
+            :is-video="true"
+            :default-value="this.video"
+            @changed-value="setOpts($event, 'video')"
+            @upload="upload($event, 'video')"/>
     </page-template>
 </template>
 
 <script>
 import PageTemplate from '../containers/PageTemplate.vue'
+import { PageSection } from '../components/ui'
+import { SeoGroup } from '../components/ui'
 import 'video.js/dist/video-js.css'
 import { videoPlayer } from 'vue-video-player'
 
@@ -45,80 +26,42 @@ export default {
     name: 'HomePage',
     components: {
         PageTemplate,
+        PageSection,
+        SeoGroup,
         videoPlayer,
     },
     data: function() {
         return {
-            errorMessage: 'Formato del file sbagliato, solo mp4',
-            video: null,
-            playerOptions: {
-                sources: [],
-            }
+            video: {
+                img: null
+            },
         }
     },
     methods: {
-        filesChange: function(fieldName, filesList) {
-            // resetto la variabile prima di riassegnare il file
-            this.video = null
-
-            let file = filesList[0]
-            let name = file.name
-
-            // prendo l'estensione dal nome del file
-            let re = /(?:\.([^.]+))?$/
-            let ext = re.exec(name)[1]
-
-            if (ext == 'mp4') {
-
-                // assegno il file alla variabile per il form
-                this.video = file
-
-                // segno il file come corretto
-                this.setValid()
-                return false
-            }
-
-            this.setInvalid('Formato del file sbagliato, solo mp4')
-            return false
-        },
-        upload: function(event) {
-            event.preventDefault()
-            if (this.video) {
+        upload: function(obj, value) {
+            if (obj.img && obj.img != '/storage/') {
                 let data = new FormData()
-                data.append('video', this.video)
+                let isFile = typeof obj.img.name == 'string'
+                if (isFile) {
+                    data.append('video', obj.img)
+                }
                 this.$http.post('/api/admin/homepage/upload-video', data).then(response => {
-                    let video = {
-                        type: 'video/mp4',
-                        src: 'storage/' + response.data.option.value
+                    this.video = {
+                        img: response.data
                     }
-                    this.playerOptions.sources.push(video)
                 })
             } else {
-                this.setInvalid('Selezionare un file')
+                this.$emit(value + '-error', 'Selezionare un file')
             }
-
         },
-        setInvalid: function(message) {
-            this.errorMessage = message
-
-            let el = this.$refs.input
-            el.classList.remove('is-valid')
-            el.classList.add('is-invalid')
-        },
-        setValid: function() {
-            let el = this.$refs.input
-            el.classList.remove('is-invalid')
-            el.classList.add('is-valid')
+        setOpts: function(obj, value) {
+            this.video = obj
         },
         getVideo: function() {
             this.$http.get('/api/admin/homepage/get-video').then(response => {
-                console.log(response);
-                let video = {
-                    type: 'video/mp4',
-                    src: response.data
+                this.video = {
+                    img: response.data
                 }
-                this.playerOptions.sources = [video]
-
             })
         }
     },
