628fc9db8d54ba0c7cc21217a83b3ab9ecd33cb1
Angus Simons <info@simonepozzobon.com>
2019-03-18 22:32:50 +0100

    aggiunta meta title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ $seo->title }}</title>
+        <meta name="title" content="{{ $seo->title }}">
         <meta name="description" content="{{ $seo->description }}" />
 
         <meta property="og:title" content="{{ $seo->title }}">
